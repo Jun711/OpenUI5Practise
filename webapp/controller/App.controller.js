5ee81ed3dde4d346d7629728b81b999c5627da1b
Jun <cygoh@umich.edu>
2017-03-01 13:04:56 -0800

    Dialogs and Fragments

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,9 +1,6 @@
 sap.ui.define([
-   "sap/ui/core/mvc/Controller",
-   "sap/m/MessageToast",
-   "sap/ui/model/json/JSONModel",
-   "sap/ui/model/resource/ResourceModel"
-], function (Controller, MessageToast, JSONModel, ResourceModel) {
+   "sap/ui/core/mvc/Controller"
+], function (Controller) {
    "use strict";
    return Controller.extend("sap.ui.demo.wt.controller.App", {
 	  //  	onInit : function () {
@@ -21,15 +18,17 @@ sap.ui.define([
 	  //        });
 	  //        this.getView().setModel(i18nModel, "i18n");
 	  //     },
-		onShowHello : function () {
-		 // show a native JavaScript alert
-		 // alert("Hello World");
-			// read msg from i18n model
-			var oBundle = this.getView().getModel("i18n").getResourceBundle();
-			var sRecipient = this.getView().getModel().getProperty("/recipient/name");
-			var sMsg = oBundle.getText("helloMsg", [sRecipient]);
-			// show message
-			MessageToast.show(sMsg);
-		}
+
+	  // moved to HelloPanel.controller
+		// onShowHello : function () {
+		//  // show a native JavaScript alert
+		//  // alert("Hello World");
+		// 	// read msg from i18n model
+		// 	var oBundle = this.getView().getModel("i18n").getResourceBundle();
+		// 	var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+		// 	var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+		// 	// show message
+		// 	MessageToast.show(sMsg);
+		// }
    });
 });
